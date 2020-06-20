4b885614328969aa2f59ffd0515bdf3206a30cf7
cavalier111 <rklemchek@yahoo.com>
2020-06-20 17:06:13 -0400

    took out scaling based on expected area, changed to recursive solution that continuously increases font size until works start overflowing, cleaned up and commented

--- imessage/static/admin/js/wordcloud.js
+++ imessage/static/admin/js/wordcloud.js
@@ -1,82 +1,81 @@
 
 $(document).ready(function(){
 	var frequencyList = JSON.parse(document.getElementById('frequency-list').textContent);
-	// frequencyList.push([String.fromCodePoint(0x1F618), 44])
- // var frequencyList = [];
- // frequencyList.push({"text":"abcd", value: 1});
- //  frequencyList.push({"text":"abcd", value: 1});
+	// frequencyList.push({"text": String.fromCodePoint(0x1F618), "value": 10})
 
- // frequencyList.push({"text":"abcd", value: 1});
+	 //generating test data randomly
+	// for (var i = 0; i <500; i++) { 
+	// 	 // frequencyList.push({"text":Math.random().toString(36).substring(3), value: Math.floor(Math.random() * 6)});
+	// }
+	//generating test data manually
+ 	// frequencyList.push({"text":"abcd", value: 50});
+ 
 
 
- // frequencyList.push({"text":"abcd", value: 1});
- // frequencyList.push({"text":"abcd", value: 1});
- // frequencyList.push({"text":"abcd", value: 1});
- // frequencyList.push({"text":"abcd", value: 1});
- // frequencyList.push({"text":"abcd", value: 1});
-
-	$("#canvas").mouseout(function(){
-  		$(".count-box").tooltip('hide');
-	});
-
+ 	//set margins
 	var margin = {top: 20, right: 20, bottom: 40, left: 20},
-    width = 950 - margin.left - margin.right,
+    width = 1200 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
-	var maxSize = d3.max(frequencyList, function (d) {return d.value;});
-	var fontSizeScale = d3.scaleLinear().domain([0,1]).range([ 0, 100]);
-	var expectedArea = d3.sum(frequencyList, function (d) {
-		// console.log((d.text.length*(fontSizeScale(d.value/maxSize) * .6  * fontSizeScale(d.value/maxSize) * 1.15)));
-		// const x =   (d.text.length*(fontSizeScale(d.value/maxSize) * .6  * fontSizeScale(d.value/maxSize) * 1.15));
-		const x = .9 ** (-.00278 * (d.text.length*(fontSizeScale(d.value/maxSize) * .6  * fontSizeScale(d.value/maxSize) * 1.15)) -20);
-		// console.log(x);
-		return x;
-	});
-	// var expectedStd = d3.deviation(frequencyList, function (d) {return d.text.length*(fontSizeScale(d.value/maxSize) * .6  * fontSizeScale(d.value/maxSize) * 1.15)});
-	var canvasSize = 950 * 450;
-	var scaleVal =(canvasSize/expectedArea); // - (1/expectedStd)*300;
-	// console.log(scaleVal, 1/expectedStd);
-	scaleVal = scaleVal > 1 ? 1 : scaleVal;
-  	var color = d3.scaleBand().range(["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854"]);
-var tooltip = d3.select("body")
-    .append("div")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .style("background-color", "black")
-    // .style("width", "120px")
-    .style("color", "#fff")
-    .style("text-align", "center")
-    .style("border-radius", "6px")
-    .style("padding", "5px 0")
-
-  	var layout = d3.layout.cloud()
-      .timeInterval(10)
-      .size([width, height])
-      .words(frequencyList)
-      .rotate(function(d) { return 0; })
-      .font('monospace')
-      .fontSize(function (d) {
-        return scaleVal * fontSizeScale(d.value/maxSize) ;
-      })
-
-      .spiral("archimedean")
-      .on("end", draw)
-      .start();
-
-  	var svg = d3.select("#my_dataviz").append("svg")
+    //set up svg
+	var svg = d3.select("#my_dataviz").append("svg")
   		.attr("width", width + margin.left + margin.right)
 	 	.attr("height", height + margin.top + margin.bottom)
 		.append("g")
-	 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-	          // .attr("transform", "translate(370,155)")
-;
+	 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+	//set up wordcloud
   	var wordcloud = svg.append("g")
       .attr('class','wordcloud')
       .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
-
+	
+	//function for text color 
 	var fill = d3.scaleOrdinal(d3.schemeCategory20);
+
+	//tooltip to show word count on hover
+	var tooltip = d3.select("body")
+	    .append("div")
+	    .style("position", "absolute")
+	    .style("z-index", "10")
+	    .style("visibility", "hidden")
+	    .style("background-color", "black")
+	    // .style("width", "120px")
+	    .style("color", "#fff")
+	    .style("text-align", "center")
+	    .style("border-radius", "6px")
+	    .style("padding", "5px 0")
+
+	var maxLayout;
+
+	//recursive function to find maximum layout of cloud that fits all words
+	function findMaxLayout(max_font_size) {
+		var maxSize = d3.max(frequencyList, function (d) {return d.value;});
+		var fontSizeScale = d3.scaleLinear().domain([0,1]).range([ 0, max_font_size]);
+  		const layout = d3.layout.cloud();
+  		layout
+	        .size([width, height])
+	      	.words(frequencyList)
+      		.rotate(function(d) { return 0; })
+       		.text(function(d) { return d.text; }) 
+      		.font('monospace')
+      		.fontSize(function (d) {
+      			//fin how big the word is compared to the biggest, scale from 0 to max_font_size
+      			return Math.floor(fontSizeScale(d.value/maxSize));
+     		})
+     		.spiral("archimedean")
+      		.on("end", function(output) {
+      			//if all the words are in the wordcliud output, the font is less than 100
+	      		if ((frequencyList.length <= output.length) && (max_font_size < 100)) {  // compare between input ant output
+			        // set the maximum sized layout to the current
+			        maxLayout = layout;
+			        // try drawing again with 5 bigger font size
+			       	return findMaxLayout(max_font_size + 5);
+	      		}
+	      		//otherwise, max size has been found, return out
+	       		else { return undefined; }
+      		})
+      		.start()
+	}
 
 	function draw(words) {
 	    wordcloud.selectAll("text")
@@ -84,15 +83,20 @@ var tooltip = d3.select("body")
 	      	.enter().append("text")
 	        .attr('class','word')
 	        .style("font-size", function(d) { return d.size + "px"; })
-	        .style("font-family", function(d) { return d.font; })
+			.style('font-family', 'monospace')
 	       	.style("fill", (d, i) => fill(i))
 	        .attr("text-anchor", "middle")
 	        .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
 	        .text(function(d) { return d.text; })
 	        .on("mouseover", function(d){tooltip.text(d.text + " was used " + d.value + " times"); return tooltip.style("visibility", "visible");})
-	      .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-40)+"px").style("left",(d3.event.pageX)+"px");})
-	      .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+	      	.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-40)+"px").style("left",(d3.event.pageX)+"px");})
+	      	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 	  };
 
+	findMaxLayout(5);
+
+	maxLayout
+		.on("end", draw)
+		.start();
 
 });
