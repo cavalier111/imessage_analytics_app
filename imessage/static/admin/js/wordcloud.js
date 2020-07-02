2d17adbf21f203967ecc51d4700ef2cae179f50d
cavalier111 <rklemchek@yahoo.com>
2020-07-01 21:45:48 -0400

    added button to reset zoom to original, added button that downloads wordcloud

--- imessage/static/admin/js/wordcloud.js
+++ imessage/static/admin/js/wordcloud.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	// var frequencyList = JSON.parse(document.getElementById('frequency-list').textContent);
+	var frequencyList = JSON.parse(document.getElementById('frequency-list').textContent);
 
 	 //generating test data randomly
 	// for (var i = 0; i <1000; i++) { 
@@ -24,15 +24,21 @@ $(document).ready(function(){
     width = 1200 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
+
+	function zoomed() {
+	  svg.attr("transform", d3.event.transform)
+	}
+	var zoom = d3.zoom().on("zoom", zoomed);
+
     //set up svg
 	var svg = d3.select("#my_dataviz").append("svg")
   		.attr("width", width + margin.left + margin.right)
 	 	.attr("height", height + margin.top + margin.bottom)
-	 	.call(d3.zoom().on("zoom", function () {
-       		svg.attr("transform", d3.event.transform)
-    	}))
+	 	.attr("id", "svg")
+	 	.call(zoom)
 		.append("g")
 	 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 	//set up wordcloud
   	var wordcloud = svg.append("g")
       .attr('class','wordcloud')
@@ -90,6 +96,7 @@ $(document).ready(function(){
 
 	function draw(words) {
 		$("#loader").hide();
+		$("#buttons").css("visibility","visible");
 	    let data = wordcloud.selectAll("text")
 	        .data(words)
 	      	.enter().append("text")
@@ -97,9 +104,9 @@ $(document).ready(function(){
         		return "translate(" + [t.x, t.y] + ")rotate(" + t.rotate + ")"
    			}).on('end', function() {
      			 d3.selectAll("text")
-     			 .on("mouseover", function(d){tooltip.text(d.text + " was used " + d.value + " times"); return tooltip.style("visibility", "visible");})
-		      	.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-40)+"px").style("left",(d3.event.pageX)+"px");})
-		      	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+	     			.on("mouseover", function(d){tooltip.text(d.text + " was used " + d.value + " times"); return tooltip.style("visibility", "visible");})
+			      	.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-40)+"px").style("left",(d3.event.pageX)+"px");})
+			      	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 		     })
 	        .attr('class','word')
 	        .style("font-size", function(d) { return d.size + "px"; })
@@ -115,6 +122,41 @@ $(document).ready(function(){
 	maxLayout
 		.on("end", draw)
 		.start();
+
+
+	// reset the zoom and use html2canvas to download
+	document.getElementById("download").onclick = () => {
+		svg.transition()
+		    .duration(500)
+		    .call(zoom.transform, d3.zoomIdentity);
+		html2canvas(
+			document.querySelector("#my_dataviz"),
+			{ width: width, height: height, scale: "5", backgroundColor: "#f2f2f2"}
+		).then(function(canvas) {
+        	saveAs(canvas.toDataURL(), 'MessagesWordCloud.png');
+		});
+	}
+
+
+	function saveAs(uri, filename) {
+	    var link = document.createElement('a');
+	    if (typeof link.download === 'string') {
+	        link.href = uri;
+	        link.download = filename;
+	        document.body.appendChild(link)
+	        link.click();
+	        document.body.removeChild(link);
+
+	    } else {
+	        window.open(uri);
+    }
+}
+
+	document.getElementById("reset").onclick = () => {
+		svg.transition()
+		    .duration(500)
+		    .call(zoom.transform, d3.zoomIdentity);
+	}
 
 
 });
