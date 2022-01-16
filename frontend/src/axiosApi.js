3168070bff5c6e50c12d093d917500bf3260ea61
cavalier111 <rklemchek@yahoo.com>
2022-01-15 19:18:08 -0500

    Added base url setting logic based on env

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
-const baseURL = 'http://localhost:8000/api/'
+const baseURL = process.env.CURRENT_ENV == 'local' ? 'http://localhost:8000/api/' : 'https://message-analytics-app.herokuapp.com/api/';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
